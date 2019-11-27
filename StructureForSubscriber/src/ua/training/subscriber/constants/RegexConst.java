13a465df9b93d45710608e811ceab799158070b9
andreydashch <andreydashch@gmail.com>
2019-11-27 12:45:44 +0200

    Add constants

--- StructureForSubscriber/src/ua/training/subscriber/constants/RegexConst.java
+++ StructureForSubscriber/src/ua/training/subscriber/constants/RegexConst.java
@@ -14,4 +14,18 @@ package ua.training.subscriber.constants;
  */
 public class RegexConst {
     public static final String FIRST_NAME = "";
+    public static final String SURNAME = "";
+    public static final String PATRONYMIC = "";
+    public static final String NICK_NAME = "";
+    public static final String COMMENT = "";
+    public static final String GROUP = "";
+    public static final String HOME_TELEPHONE = "";
+    public static final String MOBIL_TELEPHONE = "";
+    public static final String EMAIL = "";
+    public static final String SKYPE = "";
+    public static final String INDEX = "";
+    public static final String CITY = "";
+    public static final String STREET = "";
+    public static final String HOUSE_NUMBER = "";
+    public static final String FLAT_NUMBER = "";
 }
---
+++ StructureForSubscriber/src/ua/training/subscriber/constants/TextConst.java
@@ -0,0 +1,8 @@
+package ua.training.subscriber.constants;
+
+public class TextConst {
+    public static final String TEXT_SEPARATOR = " ";
+    public static final String NAME_SEPARATOR = " ";
+    public static final String ADDRESS_SEPARATOR = "/";
+
+}
