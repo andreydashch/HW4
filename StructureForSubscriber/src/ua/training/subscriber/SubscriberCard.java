9255cf816400cef712599861f86f80c0fd178e5e
andreydashch <andreydashch@gmail.com>
2019-11-27 09:28:24 +0200

    Refactoring - split structure SubscriberCard to smaller structures

--- StructureForSubscriber/src/ua/training/subscriber/SubscriberCard.java
+++ StructureForSubscriber/src/ua/training/subscriber/SubscriberCard.java
@@ -5,29 +5,42 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SubscriberCard{
+    private Identity identity;
+    private Contacts contacts;
+    private Meta meta;
+
+    SubscriberCard() {
+        identity = new Identity();
+        contacts = new Contacts();
+        meta = new Meta();
+    }
+
+}
+
+class Identity {
     private Map<String, StringBuilder> name;
     private String nickName;
     private String comment;
     private String group;
 
-    private String telephoneHome;
-    private String telephoneMobil1;
-    private String telephoneMobil2;
-
-    private String email;
-    private String skype;
-    private Map<String, StringBuilder> address;
-
-    private LocalDateTime timeOfCreate;
-    private LocalDateTime timeOfLastUpdate;
-
-    SubscriberCard(){
+    Identity() {
         name = new HashMap<String, StringBuilder>();
-        address = new HashMap<String, StringBuilder>();
 
         name.put("surname", new StringBuilder());
         name.put("firstName", new StringBuilder());
         name.put("patronymic", new StringBuilder());
+    }
+}
+
+class Contacts {
+    private TelephonesNumbers telephonesNumbers;
+    private String email;
+    private String skype;
+    private Map<String, StringBuilder> address;
+
+    Contacts() {
+        telephonesNumbers = new TelephonesNumbers();
+        address = new HashMap<String, StringBuilder>();
 
         address.put("index", new StringBuilder());
         address.put("city", new StringBuilder());
@@ -35,5 +48,15 @@ public class SubscriberCard{
         address.put("houseNumber", new StringBuilder());
         address.put("flatNumber", new StringBuilder());
     }
+}
 
+class TelephonesNumbers {
+    private String Home;
+    private String Mobil1;
+    private String Mobil2;
+}
+
+class Meta {
+    private LocalDateTime timeOfCreate;
+    private LocalDateTime timeOfLastUpdate;
 }
