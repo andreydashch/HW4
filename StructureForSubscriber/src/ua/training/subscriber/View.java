2a23d4cc2784e4135779f6c4975c71b25756e25f
andreydashch <andreydashch@gmail.com>
2019-11-27 11:45:59 +0200

    Add View

---
+++ StructureForSubscriber/src/ua/training/subscriber/View.java
@@ -0,0 +1,20 @@
+/*
+ * @(#) View.java
+ *
+ * This software can be used by anyone
+ * with no limit. But developer do not
+ * granite its proper working.
+ */
+
+
+package ua.training.subscriber;
+
+/**
+ * @author      Dashchyk Andrey
+ */
+public class View {
+
+    public void printMessage(String message){
+        System.out.println(message);
+    }
+}
