ca4e9ba59e11ee0e7da8d80e1c5745057f9d70ca
andreydashch <andreydashch@gmail.com>
2019-11-27 16:13:31 +0200

    Set RegexConst

--- StructureForSubscriber/src/ua/training/subscriber/globalconstants/RegexConst.java
+++ StructureForSubscriber/src/ua/training/subscriber/globalconstants/RegexConst.java
@@ -13,21 +13,21 @@ package ua.training.subscriber.globalconstants;
  * @author      Dashchyk Andrey
  */
 public class RegexConst {
-    public static final String FIRST_NAME = ".";
-    public static final String SURNAME = ".";
-    public static final String PATRONYMIC = ".";
-    public static final String NICK_NAME = ".";
-    public static final String COMMENT = ".";
-    public static final String GROUP = ".";
-    public static final String HOME_TELEPHONE = ".";
-    public static final String MOBIL_TELEPHONE = ".";
-    public static final String EMAIL = ".";
-    public static final String SKYPE = ".";
-    public static final String INDEX = ".";
-    public static final String CITY = ".";
-    public static final String STREET = ".";
-    public static final String HOUSE_NUMBER = ".";
-    public static final String FLAT_NUMBER = ".";
+    public static final String FIRST_NAME = "[A-Z][a-z]?";
+    public static final String SURNAME = "[A-Z][a-z]?";
+    public static final String PATRONYMIC = "[A-Z][a-z]?";
+    public static final String NICK_NAME = "\\w*";
+    public static final String COMMENT = ".{1,1000}";
+    public static final String GROUP = ".*";
+    public static final String HOME_TELEPHONE = "\\+[\\d]{3,12}";
+    public static final String MOBIL_TELEPHONE = "\\+[\\d]{3,12}";
+    public static final String EMAIL = ".*@.*?";
+    public static final String SKYPE = ".*";
+    public static final String INDEX = "[\\d]{3}[1-9]";
+    public static final String CITY = "\\w*";
+    public static final String STREET = "\\w*";
+    public static final String HOUSE_NUMBER = "[\\d]{1,4}[1-9]";
+    public static final String FLAT_NUMBER = "[\\d]{1,5}[1-9]";
 
     public static String[] getRegexArray() {
 
