1e2c9f258291832ff27bb141cbbaa56ec0c10ec6
andreydashch <andreydashch@gmail.com>
2019-11-27 09:58:31 +0200

    Adding setters

--- StructureForSubscriber/src/ua/training/subscriber/SubscriberCard.java
+++ StructureForSubscriber/src/ua/training/subscriber/SubscriberCard.java
@@ -15,38 +15,73 @@ public class SubscriberCard{
         meta = new Meta();
     }
 
+
 }
 
 class Identity {
-    private Map<String, StringBuilder> name;
+    private Name name;
     private String nickName;
     private String comment;
     private String group;
 
     Identity() {
-        name = new HashMap<String, StringBuilder>();
-
-        name.put("surname", new StringBuilder());
-        name.put("firstName", new StringBuilder());
-        name.put("patronymic", new StringBuilder());
+        name = new Name();
     }
 }
 
+class Name {
+    String surname;
+    String firstName;
+    String patronymic;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
 class Contacts {
-    private TelephonesNumbers telephonesNumbers;
+    public TelephonesNumbers telephonesNumbers;
     private String email;
     private String skype;
-    private Map<String, StringBuilder> address;
+    private Map<String, String> address;
 
     Contacts() {
         telephonesNumbers = new TelephonesNumbers();
-        address = new HashMap<String, StringBuilder>();
+        address = new HashMap<String, String>();
+    }
 
-        address.put("index", new StringBuilder());
-        address.put("city", new StringBuilder());
-        address.put("street", new StringBuilder());
-        address.put("houseNumber", new StringBuilder());
-        address.put("flatNumber", new StringBuilder());
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSkype(String skype) {
+        this.skype = skype;
+    }
+
+    public void setAddressIndex(String index) {
+        this.address.put("index", index);
+    }
+
+    public void setAddressCity(String city) {
+        this.address.put("city", city);
+    }
+
+    public void setAddressStreet(String street) {
+        this.address.put("street", street);
+    }
+
+    public void setAddressHouse(String house) {
+        this.address.put("house", house);
+    }
+    public void setAddressFlatNumber(String flatNumber) {
+        this.address.put("flatNumber", flatNumber);
     }
 }
 
@@ -54,9 +89,49 @@ class TelephonesNumbers {
     private String Home;
     private String Mobil1;
     private String Mobil2;
+
+    public void setHome(String home) {
+        Home = home;
+    }
+
+    public void setMobil1(String mobil1) {
+        Mobil1 = mobil1;
+    }
+
+    public void setMobil2(String mobil2) {
+        Mobil2 = mobil2;
+    }
+
+    public String getHome() {
+        return Home;
+    }
+
+    public String getMobil1() {
+        return Mobil1;
+    }
+
+    public String getMobil2() {
+        return Mobil2;
+    }
 }
 
 class Meta {
     private LocalDateTime timeOfCreate;
     private LocalDateTime timeOfLastUpdate;
+
+    Meta() {
+        timeOfCreate = LocalDateTime.now();
+    }
+
+    private void setTimeOfLastUpdate() {
+            timeOfLastUpdate = LocalDateTime.now();
+    }
+
+    public LocalDateTime getTimeOfCreate() {
+        return timeOfCreate;
+    }
+
+    public LocalDateTime getTimeOfLastUpdate() {
+        return timeOfLastUpdate;
+    }
 }
